98ae3ccd0908b0621a39a142ca061c0667f6cb6f
Archi Halder <archihalder23026558@gmail.com>
2022-03-01 20:57:49 +0530

    updated the files

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * rate * years / 100;
+    var future_year = new Date().getFullYear() + years;
+
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br\>" +
+        "at an interest rate of <mark>" + rate + "%</mark>,<br\>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br\>" +
+        "in the year <mark>" + future_year + "</mark>";
 }
-        
+
+function updateRate() {
+    var rv = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rv;
+}
+
+function validatePrincipal() {
+    if (principal > 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+}
